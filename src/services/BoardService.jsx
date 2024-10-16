fb1fbb30cd010d5c9a29e20e8a128396ffa93bc9
WIN-1G93LUSSCH4\ICT02_18 <asdf@gmail.com>
2024-10-16 11:13:10 +0900

    write완성

--- src/services/BoardService.jsx
+++ src/services/BoardService.jsx
@@ -8,7 +8,12 @@ const remove = (id) => {
   return http.delete(`boards/${id}`); //"boards/" + id 같음
 };
 
+const write = (data) => {
+  return http.post(`/boards/`, data); //"boards/" + id 같음
+};
+
 export default {
   getPagingList,
   remove,
+  write,
 };
